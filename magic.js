b73f57bfe69125b3ddbca49b2e84f0964f42855e
Bri Wyatt <mbriwyatt@gmail.com>
2017-01-15 01:31:55 -0600

    added 8th card to product set

--- magic.js
+++ magic.js
@@ -2,13 +2,14 @@
 
 
 var survivalKits = {
-  kindergarten: {poster: "images/kindergarten.jpg", title: "KinderGarten Survival Kit", price:"$40.00"},  
-  kindergartenParents: {poster:"images/kinder-parent.jpg", title:"Kindergarten Parent Survival", price:"$40.00"},
-  elementarySchool: {poster:"images/school-bus.jpg", title:"Elementary Survival Kit", price:"$40.00"},
+  kindergarten: {poster: "images/kindergarten.jpg", title: "Kindergarten Kids Kit", price:"$40.00"},  
+  kindergartenParents: {poster:"images/kinder-parent.jpg", title:"Kindergarten Parent Kit", price:"$40.00"},
+  elementarySchool: {poster:"images/school-bus.jpg", title:"Elementary School Kit", price:"$40.00"},
   middleSchool: {poster:"images/middle-school.jpg", title:"Middle School Survival Kit", price:"$40.00"},
-  highSchool: {poster:"images/high-school.jpg", title:"Hight School Surivival kit", price:"$40.00"},
-  college: {poster:"images/college.jpg", title:"College Survival Kit", price:"$40.00"},
-  collegeParent: {poster:"images/college-parent.jpg", title:"College Parent Survival", price:"$40.00"},
+  highSchool: {poster:"images/high-school.jpg", title:"High School Survival Kit", price:"$40.00"},
+  college: {poster:"images/college.jpg", title:"College Student Survival Kit", price:"$40.00"},
+  collegeParent: {poster:"images/college-parent.jpg", title:"College Parent Survival Kit", price:"$40.00"},
+  codingBootCamp: {poster: "images/lines-of-code.jpg", title: "Coding Boot Camp Survival Kit", price:"$40.00"}
 }
 
 var productSection = document.getElementById("productCards");
@@ -22,7 +23,7 @@ for (var key in survivalKits) {
     var price = product["price"];
     console.log(poster, title, price);
 
-	  var card = "<article class='edukit'>" + "<h2>" + title + "</h2>" + "<div class='resize'><img class='resize' src='" + poster + "'></div>" + "<footer><p>" + price + "</p></footer></article>";
+	  var card = "<article class='edukit'>" + "<h2>" + title + "</h2>" + "<div class='resize'><img class='resize' src='" + poster + "'></div>" + "<footer class><p>" + price + "</p></footer></article>";
 		productSection.innerHTML += card;
 }
 
