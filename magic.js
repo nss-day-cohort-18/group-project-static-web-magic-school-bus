f3b962696306a88dfa5edc390faf11571f1bf58b
Tamela Lerma <tlerma83@gmail.com>
2017-01-15 14:52:54 -0600

    Gave product cards dimensions, reused some photos so they would all fit within conatiners

--- magic.js
+++ magic.js
@@ -1,9 +1,9 @@
 var survivalKits = {
-  kindergarten: {poster: "images/kindergarten.jpg", title: "KinderGarten Survival Kit", price:"$40.00"}, 
-  kindergartenParents: {poster:"\\images\\kinder-parent.jpg", title:"Kindergarten Parent Survival", price:"$40.00"},
+  kindergarten: {poster: "images//school-bus.jpg", title: "KinderGarten Kit", price:"$40.00"}, 
+  kindergartenParents: {poster:"images//school-bus.jpg", title:"Kinder-Parent Survival", price:"$40.00"},
   elementarySchool: {poster:"images//school-bus.jpg", title:"Elementary Survival Kit", price:"$40.00"},
-  middleSchool: {poster:"images//middle-school.jpg", title:"Middle School Survival Kit", price:"$40.00"},
-  highSchool: {poster:"images//high-school.jpg", title:"Hight School Surivival kit", price:"$40.00"},
+  middleSchool: {poster:"images//middle-school.jpg", title:"Middle School Survival", price:"$40.00"},
+  highSchool: {poster:"images//high-school.jpg", title:"High School Surivival kit", price:"$40.00"},
   college: {poster:"images//college.jpg", title:"College Survival Kit", price:"$40.00"},
   collegeParent: {poster:"images//college-parent.jpg", title:"College Parent Survival", price:"$40.00"},
 }
@@ -23,7 +23,7 @@ for (var key in survivalKits) {
 
 	var card = "<article class='edukit'>" + "<h2>" + title + "</h2>" + "<div class='resize'><img class='resize' src='" + poster + "'></div>" + "<footer><p>" + price + "</p></footer></article>";
 
-    var card = "<article class='edukit'>" + "<h2>" + title + "</h2>" + "<div class='resize'><img class='resize' src='" + poster + "'></div>" + "<footer><p>" + price + "</p></footer></article>";
+    var card = "<article class='edukit'>" + "<h4>" + title + "</h4>" + "<div class='resize'><img class='resize' src='" + poster + "'></div>" + "<footer><p>" + price + "</p></footer></article>";
 
     productSection.innerHTML += card;
 }
